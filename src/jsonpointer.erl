95a7b8f62d1c64d779ee2851b0137d41f3b616f5
Jan Lehnardt <jan@apache.org>
2011-07-31 13:17:44 -0700

    support keys with escaped slashes and matching the root element

--- src/jsonpointer.erl
+++ src/jsonpointer.erl
@@ -6,18 +6,25 @@
 -compile(export_all).
 -endif.
 
+get(Json, "/") -> Json;
+get(Json, <<"/">>) -> Json;
 get(Json, PointerString) ->
     Pointer = parse_pointer(PointerString),
     traverse(Json, Pointer).
 
 parse_pointer(PointerString) ->
-    [_Empty | Pointer] = re:split(PointerString, "/"),
-    Pointer.
+    [_Empty | EscapedPointer] = re:split(PointerString, "/"),
+    lists:map(fun(Elm) ->
+        case re:replace(Elm, "%2f", "/") of
+        Elm -> Elm;
+        Replaced -> list_to_binary(Replaced)
+        end
+    end, EscapedPointer).
 
-traverse(Json, []) ->
-    Json;
 traverse({Key, Value}, [PointerHead | PointerRest]) when PointerHead =:= Key->
     traverse(Value, PointerRest);
+traverse(Json, []) ->
+    Json;
 traverse(Json, [PointerHead | PointerRest]) when is_list(Json) ->
     ListIndex = list_to_integer(binary_to_list(PointerHead)) + 1,
     if ListIndex > length(Json) ->
--- test/jsonpointer_tests.erl
+++ test/jsonpointer_tests.erl
@@ -16,12 +16,14 @@ simple_test() ->
         ]}}
     ]},
 
+    ?assertEqual(Json, jsonpointer:get(Json, <<"/">>)),
     ?assertEqual(1, jsonpointer:get(Json, <<"/a">>)),
     ?assertEqual(2, jsonpointer:get(Json, <<"/b/c">>)),
     ?assertEqual(3, jsonpointer:get(Json, <<"/d/e/0/a">>)),
     ?assertEqual(4, jsonpointer:get(Json, <<"/d/e/1/b">>)),
     ?assertEqual(5, jsonpointer:get(Json, <<"/d/e/2/c">>)),
 
+    ?assertEqual(Json, jsonpointer:get(Json, "/")),
     ?assertEqual(1, jsonpointer:get(Json, "/a")),
     ?assertEqual(2, jsonpointer:get(Json, "/b/c")),
     ?assertEqual(3, jsonpointer:get(Json, "/d/e/0/a")),
@@ -31,4 +33,11 @@ simple_test() ->
     ?assertThrow({error, "Pointer not found"}, jsonpointer:get(Json, "/e")),
     ?assertThrow({error, "List index > list length"},
         jsonpointer:get(Json, "/d/e/4/c")),
+    ok.
+
+complex_keys_test() ->
+    Json = {[
+        {<<"a/b">>, 1}
+    ]},
+    ?assertEqual(1, jsonpointer:get(Json, <<"/a%2fb">>)),
     ok.
